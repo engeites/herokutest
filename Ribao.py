1541c02fedb49d32e5f8e8d8ba82f9c6add4cdbb
engeites <67875617+engeites@users.noreply.github.com>
2021-06-05 00:42:22 +0300

    Update Ribao.py

--- Ribao.py
+++ Ribao.py
@@ -15,7 +15,6 @@ b = ''
 @dp.message_handler(commands=['count'])
 async def stop_it(message: types.Message):
     global a
-    await bot.send_message(admin_id, f"len(a) = {len(a)}")
     try:
         data_by_lines = split_data_by_new_line(a)
         summ = count_all(data_by_lines) - 1
@@ -44,10 +43,11 @@ async def stop_it(message: types.Message):
 
 
 @dp.message_handler(commands=['null_it'])
-async def null_a_var(message: types.Message)
+async def null_a_var(message: types.Message):
     global a
     a = ""
     await message.answer("a was set to 0")
+
 
 @dp.message_handler(commands=['help'])
 async def send_rules(message: types.Message):
